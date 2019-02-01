643543ac8a9306e7750379c3cceb541bdb0478bf
Maksim Zaretski <aramorchi@gmail.com>
2019-02-01 16:23:40 +0300

    Maksim Zaretski: Added first html pages, servlets for it and server starting code

--- src/main/java/userInterface/controller/Main.java
+++ src/main/java/userInterface/controller/Main.java
@@ -1,4 +1,25 @@
 package userInterface.controller;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 public class Main {
+    public static void main(String[] args) {
+        WebAppContext context = new WebAppContext();
+        context.setDescriptor("/webapp/WEB-INF/web.xml");
+        context.setResourceBase("/webapp");
+        context.setContextPath("/");
+        context.setParentLoaderPriority(true);
+
+        Server server = new Server(8080);
+
+        server.setHandler(context);
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
